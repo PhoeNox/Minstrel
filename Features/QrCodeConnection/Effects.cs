37bfea661c5b66d77ca222e689565f904d8750ff
Thomas Eggerling <thomas.eggerling@posteo.de>
2025-09-20 11:39:09 +0200

    Use HTTP instead of HTTPS for QR code connection string

--- Features/QrCodeConnection/Effects.cs
+++ Features/QrCodeConnection/Effects.cs
@@ -8,7 +8,7 @@ public class Effects(INetworkProvider networkProvider)
 	public Task Initialize(StoreInitializedAction action, IDispatcher dispatcher)
 	{
 		var localIpAddress = networkProvider.GetLocalIpAddress();
-		dispatcher.Dispatch(new SetConnectionStringAction($"https://{localIpAddress}:5000/remote"));
+		dispatcher.Dispatch(new SetConnectionStringAction($"http://{localIpAddress}:5000/remote"));
 		return Task.CompletedTask;
 	}
 }
