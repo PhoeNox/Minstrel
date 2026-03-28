f0741c2f455786c97f613e78d0f473cfcf07cfb4
Thomas Eggerling <thomas.eggerling@posteo.de>
2026-03-28 19:07:17 +0100

    Remove code duplication

--- Features/Playback/State.cs
+++ Features/Playback/State.cs
@@ -5,7 +5,15 @@ public record State
 {
 	public HashSet<Song> LoadedSongs { get; init; } = [];
 	public bool IsPlaying { get; init; }
-	public HashSet<PlayingSong> PlayingSongs { get; init; } = []; 
+	public HashSet<PlayingSong> PlayingSongs { get; init; } = [];
+
+	public bool IsSongOnOtherPlaylistAvailable(Playlists.State playlistState, GamePhases.State gamePhaseState)
+	{
+		var otherSong = gamePhaseState.Phase == GamePhase.Day
+				? playlistState.NightPlaylist.CurrentSong
+				: playlistState.DayPlaylist.CurrentSong;
+		return otherSong is not null && LoadedSongs.Contains(otherSong);
+	}
 }
 
 public record PlayingSong(Song Song, float Gain);
